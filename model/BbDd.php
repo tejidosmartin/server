ddfeebed0441f86462b557041272085f7d7cc77b
shakarr <risaaflo@gmail.com>
2022-05-19 23:02:52 +0200

    refactorizacion y arreglo de errores

--- model/BbDd.php
+++ model/BbDd.php
@@ -16,13 +16,13 @@ class BbDd
     public static function consulta(string $sql)
     {
         try {
-            [$server, $username, $password, $db] = ["localhost", "shakar", "tWbh0H#ov#RG4AJ%v", "tienda"];
-            /* $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            /* [$server, $username, $password, $db] = ["localhost", "shakar", "tWbh0H#ov#RG4AJ%v", "tienda"]; */
+            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
             $server = $url["host"];
             $username = $url["user"];
             $password = $url["pass"];
-            $db = substr($url["path"], 1); */
+            $db = substr($url["path"], 1);
 
             self::$conexion = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $username, $password);
             self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
